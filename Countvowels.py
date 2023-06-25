c3b1fa2f0841d9cbc906670ff812d40d218c210a
Kyndallfry <114108176+Kyndallfry@users.noreply.github.com>
2023-06-25 14:37:26 -0400

    Countvowels.py

--- Countvowels.py
+++ Countvowels.py
@@ -1,6 +1,17 @@
 # Brightspaceworks
-def countthevowels(v: str):
-    v = set(v.lower())
-    return sum([vowel in v for vowel in 'aeiou'])
-countthevowels("data")
+import sys
+print(sys.argv[0])
+print(sys.argv[1])
+print(sys.argv[2])
 
+
+def count_vowels(args):
+    vowels = set()
+
+    for arg in args:
+        for char in arg:
+            if char.lower() in 'aeiou':
+                vowels.add(char)
+    return len(vowels)
+
+print(count_vowels(sys.argv[1:]))
